c2b1598ef834569c566a3a39aa1aacfa5ecc1e69
nikopaah <nikopaah@gmail.com>
2023-04-24 18:06:22 -0300

    Joystick move the player + Try to make appear the joystick w/ touch

---
+++ Assets/Scripts/PlayerTouchMovement.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlayerTouchMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 JoystickSize = new Vector2 (300, 300);
+
+    [SerializeField]
+    private FloatingJoystick Joystick;
+
+    [SerializeField]
+    //private NavMeshAgente Player;
+
+    private Finger MovementFinger;
+    private Vector2 MovementAmount;
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += HandleFingerDown;
+        ETouch.Touch.onFingerUp += HandleFingerUp;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
+    }
+
+    private void OnDisable()
+    {
+        ETouch.Touch.onFingerDown -= HandleFingerDown;
+        ETouch.Touch.onFingerUp -= HandleFingerUp;
+        ETouch.Touch.onFingerMove -= HandleFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void HandleFingerMove(Finger MovedFinger)
+    {
+        if (MovedFinger == MovementFinger) 
+        {
+            Vector2 knobPosition;
+            float maxMovement = JoystickSize.x / 2f;
+            ETouch.Touch currentTouch = MovedFinger.currentTouch;
+
+            if (Vector2.Distance(
+                currentTouch.screenPosition,
+                Joystick.RectTransform.anchoredPosition
+                ) > maxMovement)
+            {
+                knobPosition = (
+                    currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition
+                    ).normalized * maxMovement;
+            }
+            else
+            { 
+                knobPosition = currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition;
+            }
+
+            Joystick.Knob.anchoredPosition = knobPosition;
+            MovementAmount = knobPosition / maxMovement;
+        }
+    }
+
+    private void HandleFingerUp(Finger LostFinger)
+    {
+        if (LostFinger == MovementFinger) 
+        {
+            MovementFinger = null;
+            Joystick.Knob.anchoredPosition = Vector2.zero;
+            Joystick.gameObject.SetActive(false);
+            MovementAmount = Vector2.zero;
+        }
+    }
+
+    private void HandleFingerDown(Finger TouchedFinger)
+    {
+        if (MovementFinger == null) { // Maybe add an "Touch Space" to check here
+            MovementFinger = TouchedFinger;
+            MovementAmount = Vector2.zero;
+            Joystick.gameObject.SetActive(true);
+            Joystick.RectTransform.sizeDelta = JoystickSize;
+            Joystick.RectTransform.anchoredPosition = ClampStartPosition(TouchedFinger.screenPosition);
+        }
+    }
+
+    // Makes the Joystick DON'T appears outside the screen
+    private Vector2 ClampStartPosition(Vector2 StartPosition)
+    {
+        if (StartPosition.x < JoystickSize.x / 2)
+        {
+            StartPosition.x = JoystickSize.x / 2;
+        }
+        if (StartPosition.y < JoystickSize.y / 2)
+        {
+            StartPosition.y = JoystickSize.y / 2;
+        }
+        else if(StartPosition.y > Screen.height - JoystickSize.y / 2) 
+        {
+            StartPosition.y = Screen.height - JoystickSize.y / 2;
+        }
+
+        return StartPosition;
+    }
+
+    private void Update()
+    {
+        /*Vector3 scaledMovement = Player.speed * Time.deltaTime * new Vector3(MovementAmount.x, 0, MovementAmount.y);
+
+        Player.transform.LookAt(Player.transform.position + scaledMovement, Vector2.up);*/
+    }
+}
